a0bf428f1fcaec3872320c120ad16836f3b36260
Ryo Watanabe <76159554+error96num@users.noreply.github.com>
2023-04-30 22:11:47 +0900

    Part8 - Add AgeScreen and AgeViewModel

--- app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.plcoding.core.data.preferences.DefaultPreferences
 import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.core.domain.use_case.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +15,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
     @Provides
     @Singleton
     fun provideSharedPreferences(
@@ -27,5 +27,11 @@ object AppModule {
     @Singleton
     fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
         return DefaultPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterOutDigitsUseCase(): FilterOutDigits {
+        return FilterOutDigits()
     }
 }
