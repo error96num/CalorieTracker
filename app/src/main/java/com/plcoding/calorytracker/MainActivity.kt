3485b59ff94a3d81c2565a35a2234b8ec6bc8956
©️Ryo <76159554+error96num@users.noreply.github.com>
2023-05-02 20:53:03 +0900

    Part10 - Activity&GoalScreen (#12)

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.navigation.compose.rememberNavController
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import com.plcoding.core.navigation.Route
+import com.plcoding.onboarding_presentation.activity.ActivityScreen
 import com.plcoding.onboarding_presentation.age.AgeScreen
 import com.plcoding.onboarding_presentation.gender.GenderScreen
+import com.plcoding.onboarding_presentation.goal.GoalScreen
 import com.plcoding.onboarding_presentation.height.HeightScreen
 import com.plcoding.onboarding_presentation.weight.WeightScreen
 import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
@@ -63,16 +65,15 @@ class MainActivity : ComponentActivity() {
                                 onNavigate = navController::navigate,
                             )
                         }
+                        composable(Route.ACTIVITY) {
+                            ActivityScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GOAL) {
+                            GoalScreen(onNavigate = navController::navigate)
+                        }
                         composable(Route.NUTRIENT_GOAL) {
 
                         }
-                        composable(Route.ACTIVITY) {
-
-                        }
-                        composable(Route.GOAL) {
-
-                        }
-
                         composable(Route.TRACKER_OVERVIEW) {
 
                         }
