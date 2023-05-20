5be6febe3b0e011dd3d9ec77e95164d62692ba00
Ryo Watanabe <76159554+error96num@users.noreply.github.com>
2023-05-20 22:47:02 +0900

    Fix - SharedPreference key variables

--- core/src/main/java/com/plcoding/core/data/preferences/DefaultPreferences.kt
+++ core/src/main/java/com/plcoding/core/data/preferences/DefaultPreferences.kt
@@ -48,19 +48,19 @@ class DefaultPreferences(
 
     override fun saveCarbRatio(ratio: Float) {
         sharedPref.edit()
-            .putFloat(Preferences.KEY_CARB_RATION, ratio)
+            .putFloat(Preferences.KEY_CARB_RATIO, ratio)
             .apply()
     }
 
     override fun saveProteinRatio(ratio: Float) {
         sharedPref.edit()
-            .putFloat(Preferences.KEY_PROTEIN_RATION, ratio)
+            .putFloat(Preferences.KEY_PROTEIN_RATIO, ratio)
             .apply()
     }
 
     override fun saveFatRatio(ratio: Float) {
         sharedPref.edit()
-            .putFloat(Preferences.KEY_FAT_RATION, ratio)
+            .putFloat(Preferences.KEY_FAT_RATIO, ratio)
             .apply()
     }
 
@@ -71,9 +71,9 @@ class DefaultPreferences(
         val genderString = sharedPref.getString(Preferences.KEY_GENDER, null)
         val activityLevel = sharedPref.getString(Preferences.KEY_ACTIVITY_LEVEL, null)
         val goalType = sharedPref.getString(Preferences.KEY_GOAL_TYPE, null)
-        val carbRatio = sharedPref.getFloat(Preferences.KEY_CARB_RATION, -1f)
-        val proteinRatio = sharedPref.getFloat(Preferences.KEY_PROTEIN_RATION, -1f)
-        val fatRatio = sharedPref.getFloat(Preferences.KEY_FAT_RATION, -1f)
+        val carbRatio = sharedPref.getFloat(Preferences.KEY_CARB_RATIO, -1f)
+        val proteinRatio = sharedPref.getFloat(Preferences.KEY_PROTEIN_RATIO, -1f)
+        val fatRatio = sharedPref.getFloat(Preferences.KEY_FAT_RATIO, -1f)
 
         return UserInfo(
             gender = Gender.fromString(genderString ?: "female"),
--- core/src/main/java/com/plcoding/core/domain/preferences/Preferences.kt
+++ core/src/main/java/com/plcoding/core/domain/preferences/Preferences.kt
@@ -28,9 +28,9 @@ interface Preferences {
         const val KEY_HEIGHT = "height"
         const val KEY_ACTIVITY_LEVEL = "activity_level"
         const val KEY_GOAL_TYPE = "goal_type"
-        const val KEY_CARB_RATION = "carb_ratio"
-        const val KEY_PROTEIN_RATION = "protein_ratio"
-        const val KEY_FAT_RATION = "fat_ratio"
+        const val KEY_CARB_RATIO = "carb_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ratio"
+        const val KEY_FAT_RATIO = "fat_ratio"
         const val KEY_SHOULD_SHOW_ONBOARDING = "should_show_onboarding"
     }
 }
