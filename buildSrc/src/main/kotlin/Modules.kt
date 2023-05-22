3ab79a45633c9b49fa224f15746efdce5a20b0d4
©️Ryo <76159554+error96num@users.noreply.github.com>
2023-05-22 20:19:08 +0900

    Refactor - Project Config (#35)

---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":feature-onboarding:domain"
+    const val onboardingPresentation = ":feature-onboarding:presentation"
+
+    const val trackerData = ":feature-tracker:data"
+    const val trackerDomain = ":feature-tracker:domain"
+    const val trackerPresentation = ":feature-tracker:presentation"
+}
---
+++ feature-tracker/presentation/src/main/kotlin/com/plcoding/tracker/presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,10 @@
+package com.plcoding.tracker.presentation.tracker_overview
+
+import com.plcoding.tracker.domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): TrackerOverviewEvent()
+}
