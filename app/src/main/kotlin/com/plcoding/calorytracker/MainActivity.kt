7a4b4591160281c99f1b04768abad22975b2fde7
Ryo Watanabe <76159554+error96num@users.noreply.github.com>
2023-05-22 21:24:15 +0900

    Refactor - Define navigation arg keys as const vals

--- app/src/main/kotlin/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/kotlin/com/plcoding/calorytracker/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import com.plcoding.core.domain.preferences.Preferences
-import com.plcoding.calorytracker.navigation.Route
+import com.plcoding.calorytracker.navigation.NavigationHelper
 import com.plcoding.onboarding.presentation.activity.ActivityScreen
 import com.plcoding.onboarding.presentation.age.AgeScreen
 import com.plcoding.onboarding.presentation.gender.GenderScreen
@@ -48,66 +48,67 @@ class MainActivity : ComponentActivity() {
                     NavHost(
                         navController = navController,
                         startDestination = if(shouldShowOnboarding) {
-                            Route.WELCOME
-                        } else Route.TRACKER_OVERVIEW,
+                            NavigationHelper.Welcome.route
+                        } else NavigationHelper.TrackerOverview.route,
                         modifier = Modifier.padding(padding),
                     ) {
-                        composable(Route.WELCOME) {
+                        composable(NavigationHelper.Welcome.route) {
                             WelcomeScreen(onNextClick = {
-                                navController.navigate(Route.GENDER)
+                                navController.navigate(NavigationHelper.Gender.route)
                             })
                         }
-                        composable(Route.AGE) {
+                        composable(NavigationHelper.Age.route) {
                             AgeScreen(
                                 scaffoldState = scaffoldState,
                                 onNextClick = {
-                                    navController.navigate(Route.HEIGHT)
+                                    navController.navigate(NavigationHelper.Height.route)
                                 }
                             )
                         }
-                        composable(Route.GENDER) {
+                        composable(NavigationHelper.Gender.route) {
                             GenderScreen(onNextClick = {
-                                navController.navigate(Route.AGE)
+                                navController.navigate(NavigationHelper.Age.route)
                             })
                         }
-                        composable(Route.HEIGHT) {
+                        composable(NavigationHelper.Height.route) {
                             HeightScreen(
                                 scaffoldState = scaffoldState,
                                 onNextClick = {
-                                    navController.navigate(Route.WEIGHT)
+                                    navController.navigate(NavigationHelper.Weight.route)
                                 },
                             )
                         }
-                        composable(Route.WEIGHT) {
+                        composable(NavigationHelper.Weight.route) {
                             WeightScreen(
                                 scaffoldState = scaffoldState,
                                 onNavigate = {
-                                    navController.navigate(Route.ACTIVITY)
+                                    navController.navigate(NavigationHelper.Activity.route)
                                 },
                             )
                         }
-                        composable(Route.ACTIVITY) {
+                        composable(NavigationHelper.Activity.route) {
                             ActivityScreen(onNextClick = {
-                                navController.navigate(Route.GOAL)
+                                navController.navigate(NavigationHelper.Goal.route)
                             })
                         }
-                        composable(Route.GOAL) {
+                        composable(NavigationHelper.Goal.route) {
                             GoalScreen(onNavigate = {
-                                navController.navigate(Route.NUTRIENT_GOAL)
+                                navController.navigate(NavigationHelper.NutrientGoal.route)
                             })
                         }
-                        composable(Route.NUTRIENT_GOAL) {
+                        composable(NavigationHelper.NutrientGoal.route) {
                             NutrientGoalScreen(
                                 scaffoldState = scaffoldState,
                                 onNextClick = {
-                                    navController.navigate(Route.TRACKER_OVERVIEW)
+                                    navController.navigate(NavigationHelper.TrackerOverview.route)
                                 },
                             )
                         }
-                        composable(Route.TRACKER_OVERVIEW) {
+                        composable(NavigationHelper.TrackerOverview.route) {
                             TrackerOverviewScreen(
                                 onNavigateToSearch = { mealName, day, month, year ->
-                                    navController.navigate(Route.SEARCH + "/$mealName" +
+                                    navController.navigate(NavigationHelper.Search.route +
+                                            "/$mealName" +
                                             "/$day" +
                                             "/$month" +
                                             "/$year"
@@ -116,26 +117,30 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(
-                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            route = NavigationHelper.Search.route +
+                                    "/{${NavigationHelper.Search.mealName}}" +
+                                    "/{${NavigationHelper.Search.dayOfMonth}}" +
+                                    "/{${NavigationHelper.Search.month}}" +
+                                    "/{${NavigationHelper.Search.year}}",
                             arguments = listOf(
-                                navArgument("mealName") {
+                                navArgument(NavigationHelper.Search.mealName) {
                                     type = NavType.StringType
                                 },
-                                navArgument("dayOfMonth") {
+                                navArgument(NavigationHelper.Search.dayOfMonth) {
                                     type = NavType.IntType
                                 },
-                                navArgument("month") {
+                                navArgument(NavigationHelper.Search.month) {
                                     type = NavType.IntType
                                 },
-                                navArgument("year") {
+                                navArgument(NavigationHelper.Search.year) {
                                     type = NavType.IntType
                                 },
                             ),
                         ) {
-                            val mealName = it.arguments?.getString("mealName")!!
-                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
-                            val month = it.arguments?.getInt("month")!!
-                            val year = it.arguments?.getInt("year")!!
+                            val mealName = it.arguments?.getString(NavigationHelper.Search.mealName)!!
+                            val dayOfMonth = it.arguments?.getInt(NavigationHelper.Search.dayOfMonth)!!
+                            val month = it.arguments?.getInt(NavigationHelper.Search.month)!!
+                            val year = it.arguments?.getInt(NavigationHelper.Search.year)!!
                             SearchScreen(
                                 scaffoldState = scaffoldState,
                                 mealName = mealName,
